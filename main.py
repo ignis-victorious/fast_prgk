0a71889c7eb5a572ec2db8b0daeb36383881ee40
Emmanuele Levi <ignisvictorious@gmail.com>
2025-06-24 14:35:25 +0100

    add exahustiive example of path and query parameters

--- main.py
+++ main.py
@@ -12,6 +12,24 @@ def read_root() -> dict[str, str]:
     return {"Hello": "World"}
 
 
+# PATH Parameters + QUERY parameters - Worls with http://127.0.0.1:8000/items/123?q=erre and http://127.0.0.1:8000/items/12345678
+@app.get(path="/items/{item_id}")
+def read_item(item_id: int, q: str | None = None) -> dict[str, int | str | None]:
+    return {"item id": item_id, "query": q}
+
+
+# # PATH Parameters ONLY - works with http://127.0.0.1:8000/items/12345678
+# @app.get(path="/items/{item_id}")
+# def read_item(item_id: int) -> dict[str, int]:
+#     return {"item id": item_id}
+
+
+#  OPTIONAL params - works with http://127.0.0.1:8000/products/, http://127.0.0.1:8000/products/?skip=11&limit=9
+@app.get(path="/products/")
+def list_products(skip: int = 0, limit: int = 10) -> dict[str, int]:
+    return {"skip": skip, "limit": limit}
+
+
 # def main():
 #     print("Hello from fast-prgk!")
 
@@ -23,3 +41,8 @@ def read_root() -> dict[str, str]:
 #  Import LIBRARIES
 #  Import FILES
 #  ___________________
+
+# /fix Expression of type "None" cannot be assigned to parameter of type "str"
+#   "None" is not assignable to "str", Default value of type None is not assignable to annotated parameter type str
+
+# You should annotate the parameter q as Optional[str] (or str | None in Python 3.10+) to allow None as a valid default value.
